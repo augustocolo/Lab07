dfbd3569a9cddc779a0ea2531656122e147ab4b4
augustocolo <augusto.colongo@studenti.polito.it>
2020-04-25 18:17:44 +0200

    Creata UI

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
@@ -1,23 +1,50 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.poweroutages;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ChoiceBox<?> nercChoice;
+
+    @FXML
+    private TextField maxYearsTxt;
+
+    @FXML
+    private TextField maxHoursTxt;
+
+    @FXML
+    private Button analysisBtn;
+
+    @FXML
+    private TextArea resultTxt;
+
+    @FXML
+    void doAnalysis(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert nercChoice != null : "fx:id=\"nercChoice\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert maxYearsTxt != null : "fx:id=\"maxYearsTxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert maxHoursTxt != null : "fx:id=\"maxHoursTxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert analysisBtn != null : "fx:id=\"analysisBtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert resultTxt != null : "fx:id=\"resultTxt\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
+
